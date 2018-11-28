1efb228274fedb30ee04ac5a08c69ed5cf67335b
Humayun Rashid <humayunrashid@outlook.com>
2018-11-28 11:45:34 -0500

    Updates to side navbar, aircraft-list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,27 +4,25 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import {HttpClientModule} from '@angular/common/http';
 import { NavbarComponent } from './elements/navbar/navbar.component';
-<<<<<<< HEAD
 import { LoginComponent } from './login/login.component';
-=======
+
 import { AircraftsComponent } from './aircrafts/aircrafts.component';
 import { AircraftListComponent } from './aircrafts/aircraft-list/aircraft-list.component';
 import { AircraftDetailComponent } from './aircrafts/aircraft-detail/aircraft-detail.component';
 import { AircraftListItemComponent } from './aircrafts/aircraft-list/aircraft-list-item/aircraft-list-item.component';
->>>>>>> added new aircraft related components
+import { NavbarLeftsideComponent } from './elements/navbar-leftside/navbar-leftside.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-<<<<<<< HEAD
-    LoginComponent
-=======
+    LoginComponent,
     AircraftsComponent,
     AircraftListComponent,
     AircraftDetailComponent,
-    AircraftListItemComponent
->>>>>>> added new aircraft related components
+    AircraftListItemComponent,
+    NavbarLeftsideComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/elements/navbar-leftside/navbar-leftside.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar-leftside',
+  templateUrl: './navbar-leftside.component.html',
+  styleUrls: ['./navbar-leftside.component.css']
+})
+export class NavbarLeftsideComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
